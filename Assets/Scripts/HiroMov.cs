e432dfd54fa415abe9afca36ae346d4deb6267d2
Luis Mora <linkrandomxd@gmail.com>
2020-02-26 04:03:27 -0300

    Kurov1

---
+++ Assets/Scripts/HiroMov.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiroMov : MonoBehaviour
+{// Start is called before the first frame update
+
+    Rigidbody2D rb;
+    public float moveSpeed = 140f;
+
+    //bool faceRight = true;
+    
+    //Animator myAnim;
+
+    //Variable si puede saltar o no
+    public Transform groundCheck;
+    public float checkRadius = 0.05f;
+    public LayerMask whatIsGround;
+    bool isGrounded;
+    public float JumpForce = 5f;
+
+
+private  void FixedUpdate() {
+    isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+}
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+       HiroMoviment();
+       HiroJump();
+    }
+
+    void HiroMoviment()
+    {
+         float move = Input.GetAxisRaw("Horizontal1") * moveSpeed * Time.deltaTime;
+        rb.velocity = new Vector2(move, rb.velocity.y);
+    }
+
+        void HiroJump(){
+        if(Input.GetButtonDown("JumpH")&& isGrounded){
+
+            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+
+        }
+
+    }
+}
---
+++ Assets/Scripts/KuroMov.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KuroMov : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    Rigidbody2D rb;
+    public float moveSpeed = 140f;
+
+    //bool faceRight = true;
+    
+    //Animator myAnim;
+
+    //Variable si puede saltar o no
+    public Transform groundCheck;
+    public float checkRadius = 0.05f;
+    public LayerMask whatIsGround;
+    bool isGrounded;
+
+    public float JumpForce = 5f;
+
+
+private  void FixedUpdate() {
+    isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+}
+
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+       KuroMoviment();
+       KuroJump();
+    }
+
+    void KuroMoviment()
+    {
+         float move = Input.GetAxisRaw("Horizontal2") * moveSpeed * Time.deltaTime;
+        rb.velocity = new Vector2(move, rb.velocity.y);
+    }
+
+    void KuroJump(){
+        if(Input.GetButtonDown("JumpK")&& isGrounded){
+
+            rb.velocity = new Vector2(rb.velocity.x, JumpForce);
+
+        }
+
+    }
+
+}
